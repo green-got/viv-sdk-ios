9142e10ddef2e5b5de312177e7b1ca1a760214b6
GGBS SDK release <sdk-release@users.noreply.github.com>
2026-09-21 20:52:05 +0000

    Release VivSDK 0.3.0

--- Package.swift
+++ Package.swift
@@ -4,5 +4,5 @@ let package = Package(
     name: "VivSDK",
     platforms: [.iOS(.v14)],
     products: [.library(name: "VivSDK", targets: ["VivSDK"])],
-    targets: [.binaryTarget(name: "VivSDK", url: "https://github.com/green-got/viv-sdk-ios/releases/download/0.2.3/VivSDK.xcframework.zip", checksum: "e0bbfeb2bae905cd70f64f266c74fb1d6b2f730088afbca450f683c29d02cd98")]
+    targets: [.binaryTarget(name: "VivSDK", url: "https://github.com/green-got/viv-sdk-ios/releases/download/0.3.0/VivSDK.xcframework.zip", checksum: "d05b50d7bdf8d8862ef1eecf321c495c363e0ae393d3f5de3ab1f71b0f5761b4")]
 )
